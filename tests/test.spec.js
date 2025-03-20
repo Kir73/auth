e6262c53970e1b17bb15f4b3b52572249964f8f0
Кирилл <idkirill116@gmail.com>
2025-03-20 20:24:07 +0300

    testnew

---
+++ tests/test.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require('@playwright/test');
+
+test('Verify product title, description, price, and add to cart button', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/inventory.html');
+    const productTitle = page.locator('.inventory_item_name', { hasText: 'Sauce Labs Backpack' });
+    const productDescription = page.locator('.inventory_item_desc');
+    const productPrice = page.locator('.inventory_item_price');
+    const addToCartButton = page.locator('text=Add to cart');
+
+ 
+    await expect(productTitle).toBeVisible();
+    await expect(productTitle).toHaveText('Sauce Labs Backpack');
+
+ 
+    await expect(productDescription).toBeVisible();
+    await expect(productDescription).toHaveText(
+        'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.'
+    );
+
+  
+    await expect(productPrice).toBeVisible();
+    await expect(productPrice).toHaveText('$29.99');
+
+   
+    await expect(addToCartButton).toBeVisible();
+    await expect(addToCartButton).toHaveText('Add to cart');
+});
---
+++ tests/test3.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+
+test('Verify product details match between main page and cart', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/inventory.html');
+    const productTitle = page.locator('.inventory_item_name', { hasText: 'Sauce Labs Backpack' });
+    const productDescription = page.locator('.inventory_item_desc');
+    const productPrice = page.locator('.inventory_item_price');
+    const addToCartButton = page.locator('text=Add to cart');
+
+    const titleText = await productTitle.textContent();
+    const descriptionText = await productDescription.textContent();
+    const priceText = await productPrice.textContent();
+
+    await addToCartButton.click();
+
+    await page.locator('.shopping_cart_link').click();
+    const cartTitle = page.locator('.inventory_item_name');
+    const cartDescription = page.locator('.inventory_item_desc');
+    const cartPrice = page.locator('.inventory_item_price');
+    await expect(cartTitle).toHaveText(titleText);
+    await expect(cartDescription).toHaveText(descriptionText);
+    await expect(cartPrice).toHaveText(priceText);
+});
